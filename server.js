c48a35d2f1be2895d8c245bbb9e12907ceb089dd
Jae Kim <jaeshinkim321@gmail.com>
2020-10-09 15:10:09 -0700

    post method functions

--- server.js
+++ server.js
@@ -5,41 +5,45 @@ const fs = require("fs")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"))
 
-// Returns the index.html
-app.get("/", function(req, res){
+
+function readJSON(){
+    let dbData = fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8");
+    return JSON.parse(dbData);
+}
+
+// Get route to the index.html
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
-// Returns the notes.html
-app.get("/notes", function(req, res){
+// Get route to the notes.html
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-// returns the db.json data
-app.get("/api/notes", function(req, res){
-    let dbData = fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8");
-    dbData = JSON.parse(dbData);
+// ------------------------------------------------------------------------
 
-    res.json(dbData)
-})
-
-app.post("/api/notes", function(req, res){
-    const newNote = {
-        title: req.body.title,
-        text: req.body.text
-    }
-
-    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(newNote, null, 2));
+// Get request to get db.json data
+app.get("/api/notes", function (req, res) {
+    res.json(readJSON());
 });
 
+app.post("/api/notes", function (req, res) {
+    let dbData = readJSON();
+    let addNote = {
+        title: req.body.title,
+        text: req.body.text,
+    };
+    dbData.push(addNote)
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(dbData, null, 2));
+    res.send("Success!")
+});
 
-app.listen(PORT,function(){
+app.listen(PORT, function () {
     console.log("Listening on port " + PORT)
 });
