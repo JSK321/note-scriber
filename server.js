dfed5d41f4f7e771c0188860fdf53e76b03f914d
Jae Kim <jaeshinkim321@gmail.com>
2020-10-08 17:17:43 -0700

    routes are set!

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"))
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+
+
+app.listen(PORT,function(){
+    console.log("Listening on port " + PORT)
+});
